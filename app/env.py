4d8487e4425a7ce34210acf94c19e3b9f92425f9
fukuba-shuma <calculus.fukuba@gmail.com>
2022-07-15 19:46:02 +0900

    create get_db function

--- app/env.py
+++ app/env.py
@@ -2,3 +2,9 @@ import os
 
 
 APP_ENV = os.environ.get('APP_ENV')
+
+DB_USER = os.environ.get('MYSQL_USER')
+DB_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+DB_ROOT_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')
+DB_HOST = os.environ.get('MYSQL_HOST')
+DB_NAME = os.environ.get('MYSQL_DATABASE')
