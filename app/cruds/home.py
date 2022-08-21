d2e576703c1e0f5a6f529aec0af431692f15bcf9
fukuba-shuma <calculus.fukuba@gmail.com>
2022-08-21 20:24:23 +0900

    create home crud

--- app/cruds/home.py
+++ app/cruds/home.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import StatementError
 from fastapi import HTTPException
 
-from models import LearningTime, LookingBack, User, Week, InputCurriculum, OutputCurriculum, Week
+from models import LearningTime, LookingBack, User, Week, InputCurriculum, OutputCurriculum, UsersInputCurriculums
+from models.associations.users_output_curriculums import UsersOutputCurriculums
 from utils.logger import get_logger
 from starlette.status import HTTP_400_BAD_REQUEST
 from .domains.Week import Week as WeekDomain
@@ -18,30 +19,50 @@ def read_home(db: Session,
         user: User = db.query(User).get(user_id)
     except StatementError:
         pass
-
     if not user:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
                             detail='Invalid user id given.')
-
     week = WeekDomain.get_this_week(db=db,
                                     model=Week,
                                     entrance_date=user.posse_year.entrance_date)
 
-    # input_curriculums = db.query(InputCurriculum).filter(
-    #     InputCurriculum.week == week).all()
+    input_curriculums = db.query(InputCurriculum.curriculum_name,
+                                 UsersInputCurriculums.done)\
+        .join(UsersInputCurriculums,
+              InputCurriculum.uuid == UsersInputCurriculums.input_curriculum_id)\
+        .filter(and_(
+            InputCurriculum.week == week,
+            UsersInputCurriculums.users == user))\
+        .all()
 
-    # output_curriculums = db.query(OutputCurriculum).filter(
-    #     OutputCurriculum.week == week
-    # ).all()
+    output_curriculums = db.query(OutputCurriculum.curriculum_name,
+                                  UsersOutputCurriculums.done)\
+        .join(UsersOutputCurriculums,
+              OutputCurriculum.uuid == UsersOutputCurriculums.output_curriculum_id)\
+        .filter(and_(
+            OutputCurriculum.week == week,
+            UsersOutputCurriculums.users == user))\
+        .all()
 
     try:
-        learning_time = db.query(LearningTime).filter(and_(
-            LearningTime.user == user,
-            LearningTime.week == week
-        )).one_or_none()
+        learning_time = db.query(LearningTime)\
+            .filter(and_(
+                    LearningTime.user == user,
+                    LearningTime.week == week
+                    )).one_or_none()
     except StatementError:
         pass
 
-    return learning_time
+    looking_back = db.query(LookingBack)\
+        .filter(LookingBack.user == user,
+                LookingBack.week == week)\
+        .one_or_none()
 
-    # return output_curriculums 
+    return {
+        'input_curriculums': input_curriculums,
+        'output_curriculums': output_curriculums,
+        'learning_time': learning_time.learning_time,
+        'looking_back': looking_back
+    }
+
+    # return output_curriculums
