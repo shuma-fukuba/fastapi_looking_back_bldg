70e0f8831d2c7cabe35a855d8023a4d0ae336fc3
fukuba-shuma <calculus.fukuba@gmail.com>
2022-08-22 11:16:27 +0900

    output_curriculum api

--- app/cruds/curriculums/output_curriculums.py
+++ app/cruds/curriculums/output_curriculums.py
@@ -1,5 +1,5 @@
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import StatementError
 from fastapi import HTTPException
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 from models import OutputCurriculum, User, UsersOutputCurriculums
@@ -29,6 +29,48 @@ def get_output_curriculums(db: Session,
         raise HTTPException(status_code=HTTP_404_NOT_FOUND,
                             detail='Record not found.')
     return items
+
+
+def get_output_curriculum(db: Session,
+                          user_id: str,
+                          curriculum_id: str,
+                          model: OutputCurriculum,
+                          association_model: UsersOutputCurriculums,
+                          user_model: User):
+    user = _get_user(db=db, model=user_model, user_id=user_id)
+    try:
+        item: OutputCurriculum = db.query(model, association_model)\
+            .join(association_model, model.uuid == association_model.output_curriculum_id)\
+            .filter(and_(model.uuid == curriculum_id,
+                         association_model.users == user)).one_or_none()
+    except Exception:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
+                            detail='Unrecognized id format.')
+    if not item:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND,
+                            detail='Record not found.')
+    return item
+
+
+def udpate_curriculum_done(db: Session,
+                           user_id: str,
+                           curriculum_id: str,
+                           done: bool,
+                           model: UsersOutputCurriculums):
+    try:
+        item: UsersOutputCurriculums = db.query(model).filter(and_(
+            model.user_id == user_id,
+            model.output_curriculum_id == curriculum_id
+        )).one_or_none()
+    except Exception:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
+                            detail='Unrecognized id format.')
+    if not item:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND,
+                            detail='Record not found.')
+    item.done = done
+    db.commit()
+    return (user_id, curriculum_id)
 
 
 def _get_user(db: Session,
