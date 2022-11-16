67ff360e1862a3125df465473cea2ddd28f766a4
fukuba-shuma <calculus.fukuba@gmail.com>
2022-11-16 20:33:02 +0900

    create user crud

--- app/cruds/user.py
+++ app/cruds/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import StatementError
 from fastapi import HTTPException
-from starlette.status import HTTP_404_NOT_FOUND
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
 
 from utils.logger import get_logger
 from models import User
@@ -20,3 +20,17 @@ def read_users(db: Session, model: User):
         raise HTTPException(status_code=HTTP_404_NOT_FOUND,
                             detail='Record not found.')
     return users
+
+
+def read_user(user_id: str,
+              db: Session,
+              model: User):
+    try:
+        user = db.query(model).get(user_id)
+    except Exception:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
+                            detail='Unrecognized id formant.')
+    if not user:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND,
+                            detail='Record not found.')
+    return user
--- app/routers/user.py
+++ app/routers/user.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from cruds import user as user_crud
+from cruds import user as crud
 from database import get_db
 from models import User
 import schemas
@@ -12,4 +12,11 @@ router = APIRouter()
 
 @router.get('/', response_model=List[schemas.User])
 async def read_users(db: Session = Depends(get_db)):  # current userは認証してる
-    return user_crud.read_users(db=db, model=User)
+    return crud.read_users(db=db, model=User)
+
+
+@router.get('/{user_id}', response_model=schemas.User)
+async def read_user(user_id: str, db: Session = Depends(get_db)):
+    return crud.read_user(user_id=user_id,
+                          db=db,
+                          model=User)
